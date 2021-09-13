c1e5d23927ea328cfd4946be7073e88db64f78ad
Stanislaw Grams <stanislawgrams@gmail.com>
2021-09-14 01:16:14 +0200

    update logger script to incorporate name running in the filename

--- cortex-m33/run-logger.sh
+++ cortex-m33/run-logger.sh
@@ -5,4 +5,4 @@
 # Stanislaw Grams <stanislawgrams@gmail.com>
 # MIT License
 
-cu -l /dev/ttyACM0 -s 115200 | tee $(date +'%Y_%m_%d-%H:%M:%S').log
+cu -l /dev/ttyACM0 -s 115200 | tee cortex_m33-$(date +'%Y_%m_%d-%H:%M:%S').log
